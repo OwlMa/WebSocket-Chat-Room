2cf2805be6af32700e1ead5375bf2051da7ef096
OwlMa <matianyang0603@gmail.com>
2020-08-02 02:21:27 -0400

    setup maven

---
+++ src/test/java/com/chatroom/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
